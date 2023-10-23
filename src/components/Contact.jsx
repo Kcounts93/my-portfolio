f7487ed32af2995c5b901ca3a0f1bfcdd03ca575
Kayla <95451731+Kcounts93@users.noreply.github.com>
2023-10-22 17:51:11 -0700

    contact form added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,128 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [errors, setErrors] = useState({});
+  const [submissionSuccess, setSubmissionSuccess] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newErrors = {};
+
+    if (!formData.name) {
+      newErrors.name = "Name is required";
+    }
+
+    if (!formData.email) {
+      newErrors.email = "Email is required";
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.email)) {
+      newErrors.email = "Invalid email format";
+    }
+
+    if (!formData.message) {
+      newErrors.message = "Message is required";
+    }
+
+    if (Object.keys(newErrors).length === 0) {
+      // Simulate a successful submission (you can replace this with your actual submission logic)
+      setSubmissionSuccess(true);
+    } else {
+      setSubmissionSuccess(false);
+    }
+
+    setErrors(newErrors);
+  };
+
   return (
-    <div name="contact" className="w-full px-5 lg:px-20 mt-20 mb-20 flex-row">
+    <div name="experience" className="w-full px-5 lg:px-20 mt-20 mb-20">
       <div className="text-heading-section mb-10">
-        <h2 className="text-putty text-[3.5rem] mb-2 font-light">Contact</h2>
+        <h2 className="text-[3.5rem] mb-2 font-light">Contact</h2>
+      </div>
+      <div className="max-w-md mx-auto p-4 rounded-lg shadow-md">
+        {submissionSuccess ? (
+          <div className="text-green-500 text-center mb-4">
+            Form submitted successfully!
+          </div>
+        ) : (
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4 ">
+              <label htmlFor="name" className="block text-putty">
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                className={`w-full px-3 py-2 border ${
+                  errors.name ? "border-red-500" : "border-gray-300"
+                } rounded`}
+              />
+              {errors.name && (
+                <p className="text-red-500 text-sm mt-1">{errors.name}</p>
+              )}
+            </div>
+
+            <div className="mb-4">
+              <label htmlFor="email" className="block ">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                className={`w-full px-3 py-2 border ${
+                  errors.email ? "border-red-500" : "border-gray-300"
+                } rounded`}
+              />
+              {errors.email && (
+                <p className="text-red-500 text-sm mt-1">{errors.email}</p>
+              )}
+            </div>
+
+            <div className="mb-4">
+              <label htmlFor="message" className="block ">
+                Message
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+                rows="4"
+                className={`w-full px-3 py-2 border ${
+                  errors.message ? "border-red-500" : "border-gray-300"
+                } rounded`}
+              />
+              {errors.message && (
+                <p className="text-red-500 text-sm mt-1">{errors.message}</p>
+              )}
+            </div>
+
+            <button
+              type="submit"
+              className="bg-charcoal py-2 px-4 rounded hover:bg-putty hover:text-charcoal duration-300"
+            >
+              Submit
+            </button>
+          </form>
+        )}
       </div>
     </div>
   );
