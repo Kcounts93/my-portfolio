11a70854f242461d29b4dd92766f35aec6805c2e
Kayla <95451731+Kcounts93@users.noreply.github.com>
2023-11-24 15:33:16 -0800

    shadow and padding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,79 +29,81 @@ const Navbar = () => {
 
   return (
     <motion.div
-      className="fixed z-10 w-full h-20 px-5 lg:px-20 flex justify-between items-center bg-darkgrey font-normal"
+      className="fixed z-10 w-full h-50 bg-darkgrey shadow-xl"
       initial={{ opacity: 0, y: -100 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: -100 }}
     >
-      <div>
-        <img src={Logo} alt="my logo" />
-      </div>
-      {/* Hamburger */}
-      <div onClick={toggleNav} className="md:hidden z-10 text-2xl">
-        {nav ? (
-          <FiX className="transition-transform hover:rotate-180 hover:scale-125 text-drap duration-300" />
-        ) : (
-          <FiMenu />
-        )}
-      </div>
-      <AnimatePresence>
-        {nav && (
-          // mobile
-          <motion.div
-            className="fixed top-0 right-0 w-full md:w-1/4 h-full flex flex-col items-center justify-center bg-charcoal"
-            initial="hidden"
-            animate="visible"
-            exit="hidden"
-            variants={navVariants}
-          >
-            <ul className="text-4xl py-4 text-center font-medium">
-              {navLinks.map((link) => (
-                <motion.li
-                  key={link}
-                  className="text-md transition-transform my-5 hover:-translate-y-1 hover:scale-125 duration-300"
-                >
-                  <a href={`#${link}`} onClick={closeNav}>
-                    {link}
+      <div className="container mx-auto flex justify-between items-center p-1 lg:px-20 ">
+        <div>
+          <img src={Logo} alt="my logo" />
+        </div>
+        {/* Hamburger */}
+        <div onClick={toggleNav} className="md:hidden z-10 text-2xl">
+          {nav ? (
+            <FiX className="transition-transform hover:rotate-180 hover:scale-125 text-drap duration-300" />
+          ) : (
+            <FiMenu />
+          )}
+        </div>
+        <AnimatePresence>
+          {nav && (
+            // mobile
+            <motion.div
+              className="fixed top-0 right-0 w-full md:w-1/4 h-full flex flex-col items-center justify-center bg-charcoal"
+              initial="hidden"
+              animate="visible"
+              exit="hidden"
+              variants={navVariants}
+            >
+              <ul className="text-4xl py-4 text-center font-medium">
+                {navLinks.map((link) => (
+                  <motion.li
+                    key={link}
+                    className="text-md transition-transform my-5 hover:-translate-y-1 hover:scale-125 duration-300"
+                  >
+                    <a href={`#${link}`} onClick={closeNav}>
+                      {link}
+                    </a>
+                  </motion.li>
+                ))}
+              </ul>
+              {/* Social Media Links */}
+              <ul className="flex">
+                <li>
+                  <a
+                    href="https://www.linkedin.com/in/kaylacounts/"
+                    target="blank"
+                  >
+                    <FiLinkedin className="text-2xl transition-transform hover:scale-150 duration-300" />
                   </a>
-                </motion.li>
-              ))}
-            </ul>
-            {/* Social Media Links */}
-            <ul className="flex">
-              <li>
-                <a
-                  href="https://www.linkedin.com/in/kaylacounts/"
-                  target="blank"
-                >
-                  <FiLinkedin className="text-2xl transition-transform hover:scale-150 duration-300" />
-                </a>
-              </li>
-              <li>
-                <a href="https://github.com/Kcounts93" target="blank">
-                  <FiGithub className="text-2xl transition-transform hover:scale-150 duration-300" />
-                </a>
-              </li>
-              <li>
-                <a href="https://www.instagram.com/kaylaranae.psd/">
-                  <FiInstagram className="text-2xl transition-transform hover:scale-150 duration-300" />
-                </a>
-              </li>
-            </ul>
-          </motion.div>
-        )}
-      </AnimatePresence>
-      {/* Non Mobile Menu */}
-      <ul className="hidden md:flex">
-        {navLinks.map((link) => (
-          <li
-            key={link}
-            className="text-md transition-transform hover:-translate-y-1 hover:scale-125 duration-300"
-          >
-            <a href={`#${link}`}>{link}</a>
-          </li>
-        ))}
-      </ul>
+                </li>
+                <li>
+                  <a href="https://github.com/Kcounts93" target="blank">
+                    <FiGithub className="text-2xl transition-transform hover:scale-150 duration-300" />
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.instagram.com/kaylaranae.psd/">
+                    <FiInstagram className="text-2xl transition-transform hover:scale-150 duration-300" />
+                  </a>
+                </li>
+              </ul>
+            </motion.div>
+          )}
+        </AnimatePresence>
+        {/* Non Mobile Menu */}
+        <ul className="hidden md:flex">
+          {navLinks.map((link) => (
+            <li
+              key={link}
+              className="text-md transition-transform hover:-translate-y-1 hover:scale-125 duration-300"
+            >
+              <a href={`#${link}`}>{link}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </motion.div>
   );
 };
