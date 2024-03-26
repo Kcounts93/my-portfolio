5c5bda39a7467edbd1587fcdfc90087bd55dba14
Kayla <kcounts0211@gmail.com>
2024-03-25 17:18:50 -0700

    text sizing for mobile

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,41 +4,45 @@ const Modal = ({ project, isOpen, onRequestClose }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-60 flex justify-center items-center z-50">
-      <div className="relative bg-charcoal p-4 rounded-lg max-w-lg w-full">
+    <div className='fixed inset-0 bg-black bg-opacity-60 flex justify-center items-center z-50'>
+      <div className='relative bg-charcoal p-4 rounded-lg max-w-lg w-full'>
         {/* Close Icon in the top-right corner */}
         <button
           onClick={onRequestClose}
-          className="absolute top-0 right-0 mt-2 mr-2 text-white text-lg leading-none z-50"
+          className='absolute top-0 right-0 mt-2 mr-2 text-white text-lg leading-none z-50'
         >
-          <FiX className="text-2xl" />
+          <FiX className='text-2xl' />
         </button>
-        <div className="flex flex-col">
-          <h2 className="text-xl font-medium">{project.title}</h2>
-          <div className="h-[300px] overflow-hidden flex justify-center items-center">
-            <img src={project.image} alt={project.title} className="my-2 h-100 w-100" />
+        <div className='flex flex-col'>
+          <h2 className='text-xl font-medium'>{project.title}</h2>
+          <div className='h-[300px] overflow-hidden flex justify-center items-center'>
+            <img
+              src={project.image}
+              alt={project.title}
+              className='my-2 h-100 w-100'
+            />
           </div>
-          <p className="my-2 text-lg">About the project:</p>
-          <p className="my-2 font-light">{project.subtitle}</p>
-          <p className="my-2 text-lg ">Technologies used:</p>
-          <p className="text-lg mb-2">{project.categories.join(" | ")}</p>
-          <div className="flex">
-          <a
-            href={project.projectLink}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mr-2 underline hover:text-drap transition duration-500"
-          >
-            Code
-          </a>
-          <a
-            href={project.liveLink}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="ml-2 underline hover:text-drap transition duration-500"
-          >
-            Live Project
-          </a>
+          <p className='my-2 text-lg'>About the project:</p>
+          <p className='my-2 font-light'>{project.subtitle}</p>
+          <p className='my-2 text-lg '>Technologies used:</p>
+          <p className='text-lg mb-2'>{project.categories.join(" | ")}</p>
+          <div className='flex'>
+            <a
+              href={project.projectLink}
+              target='_blank'
+              rel='noopener noreferrer'
+              className='mr-2 underline hover:text-drap transition duration-500'
+            >
+              GitHub
+            </a>
+            <a
+              href={project.liveLink}
+              target='_blank'
+              rel='noopener noreferrer'
+              className='ml-2 underline hover:text-drap transition duration-500'
+            >
+              Live Project
+            </a>
           </div>
         </div>
       </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
-import Modal from "./Modal"; // Assuming you have a Modal component
+import Modal from "./Modal";
 
 // Project data
 import notesApp from "../assets/notes-app.png";
 import otr from "../assets/ontherocks.png";
 import gpt4 from "../assets/gpt4.png";
-// import chatBot from "../assets/chatronix.png";
 import portfolioImg from "../assets/portfolio.png";
 import kingsQuest from "../assets/kings-quest.png";
 
@@ -13,64 +12,54 @@ const projectsData = [
   {
     id: 1,
     title: "GPT-4 Landing Page",
-    subtitle: "The ChatGPT-4 Landing Page is a testament to my front-end development journey with NuCamp's React course, showcasing mastery in React, BEM for CSS organization, and custom styling without reliance on frameworks. This project highlights my dedication to learning and excellence in web development, embodying core React principles, personalized UI design, and a clean, systematic file structure for optimal code readability and scalability.",
+    subtitle:
+      "The ChatGPT-4 Landing Page is a testament to my front-end development journey with NuCamp's React course, showcasing mastery in React, BEM for CSS organization, and custom styling without reliance on frameworks. This project highlights my dedication to learning and excellence in web development, embodying core React principles, personalized UI design, and a clean, systematic file structure for optimal code readability and scalability.",
     categories: ["HTML", "CSS", "JS", "React"],
     image: gpt4,
     projectLink: "https://github.com/Kcounts93/gpt4-react-landing",
     liveLink: "https://kcounts93.github.io/gpt4-react-landing/",
-    featured: true, // Featured project
   },
 
   {
     id: 2,
     title: "On the Rocks",
-    subtitle: "The On The Rocks Mixology Academy Website showcases my first fully functional web app, leveraging Bootstrap 5.3 and SASS/SCSS for responsive design. This project was a hands-on learning experience in employing modern, basic technologies to build a mixology education platform, blending my passion for bartending with skills in creating engaging, custom web components.",
+    subtitle:
+      "The On The Rocks Mixology Academy Website showcases my first fully functional web app, leveraging Bootstrap 5.3 and SASS/SCSS for responsive design. This project was a hands-on learning experience in employing modern, basic technologies to build a mixology education platform, blending my passion for bartending with skills in creating engaging, custom web components.",
     categories: ["HTML", "CSS", "JS", "Bootstrap", "SASS"],
     image: otr,
     projectLink: "https://github.com/Kcounts93/on-the-rocks",
     liveLink: "https://ontherocksmixology.netlify.app/",
-    featured: true, // Featured project
-  },  
+  },
   {
     id: 3,
     title: "The King's Quest",
-    subtitle: "The King's Quest is a choose-your-own-adventure game crafted with vanilla HTML, CSS, and JavaScript. Emphasizing decision-driven narratives, it leverages structured object-oriented programming for dynamic story progression, offering players a unique, interactive storytelling experience.",
+    subtitle:
+      "The King's Quest is a choose-your-own-adventure game crafted with vanilla HTML, CSS, and JavaScript. Emphasizing decision-driven narratives, it leverages structured object-oriented programming for dynamic story progression, offering players a unique, interactive storytelling experience.",
     categories: ["HTML", "CSS", "JS"],
     image: kingsQuest,
     projectLink: "https://github.com/Kcounts93/kings-quest-cyoa",
     liveLink: "https://kcounts93.github.io/kings-quest-cyoa//",
-    featured: true, // Featured project
   },
-  // {
-  //   id: 4,
-  //   title: "Chatronix AI Chatbot ",
-  //   subtitle: "Snippet",
-  //   categories: ["Bootstrap", "PHP", "JS"],
-  //   image: chatBot,
-  //   projectLink: "https://github.com/Kcounts93/php-chatbot",
-  //   liveLink: "https://chatronix.kaylacounts.com/",
-  //   featured: false, // Featured project
-  // },
   {
     id: 5,
     title: "Category Notes App",
-    subtitle: "The React Category Notes App is a testament to my growing expertise in React and state management, enriched by integrating Bootstrap 5.3 for a responsive layout. This project was a deep dive into utilizing local storage for data persistence and advancing my understanding of state management techniques. Through developing functionalities to filter, search, and tag notes, I gained valuable insights into creating more organized and user-friendly applications. This experience has significantly bolstered my skills and enthusiasm for tackling more complex projects in the future.",
+    subtitle:
+      "The React Category Notes App is a testament to my growing expertise in React and state management, enriched by integrating Bootstrap 5.3 for a responsive layout. This project was a deep dive into utilizing local storage for data persistence and advancing my understanding of state management techniques. Through developing functionalities to filter, search, and tag notes, I gained valuable insights into creating more organized and user-friendly applications. This experience has significantly bolstered my skills and enthusiasm for tackling more complex projects in the future.",
     categories: ["HTML", "CSS", "JS", "Bootstrap", "React"],
     image: notesApp,
     projectLink: "https://github.com/Kcounts93/react-notes-app",
     liveLink: "https://kcounts93.github.io/react-notes-app/",
-    featured: false, // Featured project
   },
 
   {
     id: 6,
     title: "Front End Portfolio",
-    subtitle: "My personal portfolio website has been one of the biggest sources of learning. It has gone through many iterations and so much growth. I have learned from this more than anything so far. It has challenged me to try new things, read an un-godly amount of documentation and expand my horizons on what I can do. I built this using Tailwind and React. I never used Tailwind before this and now I love it. I've learned state management and more on React hooks. It's been such a fun journey that will truly never end.",
+    subtitle:
+      "My personal portfolio website has been one of the biggest sources of learning. It has gone through many iterations and so much growth. I have learned from this more than anything so far. It has challenged me to try new things, read an un-godly amount of documentation and expand my horizons on what I can do. I built this using Tailwind and React. I never used Tailwind before this and now I love it. I've learned state management and more on React hooks. It's been such a fun journey that will truly never end.",
     categories: ["HTML", "CSS", "JS", "Tailwind", "React"],
     image: portfolioImg,
-    projectLink: "#",
+    projectLink: "https://github.com/Kcounts93/my-portfolio",
     liveLink: "#",
-    featured: false, // Featured project
   },
 ];
 
@@ -95,27 +84,47 @@ const Projects = () => {
   };
 
   return (
-    <div id="Projects" className="section" >
-    <div className="container mx-auto px-5 mb-28 md:mb-28 md:px-10 lg:px-20 mt-20 h-[80vh]">
-    <h2 className="text-4xl mb-10 font-light">Projects</h2>
-      <div className="grid grid-cols-3 gap-4 h-full">
-        {projectsData.map((project, index) => (
-          <div key={project.id} className={`${projectClasses[index % projectClasses.length]} shadow-lg rounded-lg overflow-hidden group relative`}>
-            <img src={project.image} alt={project.title} className="w-full h-full object-cover"/>
-            <div className="absolute inset-0 bg-charcoal backdrop-filter backdrop-blur-md bg-opacity-60 opacity-0 group-hover:opacity-100 flex justify-center items-center duration-500 ease-in-out">
-              <div className="text-center text-putty">
-                <p className="text-xl font-medium">{project.title}</p>
-                <p className="mt-2">{project.categories.join(', ')}</p>
+    <div id='Projects' className='section'>
+      <div className='container mx-auto px-5 mb-28 md:mb-28 md:px-10 lg:px-20 mt-20 h-[80vh]'>
+        <h2 className='text-4xl mb-10 font-light'>Projects</h2>
+        <div className='grid grid-cols-3 gap-4 h-full'>
+          {projectsData.map((project, index) => (
+            <div
+              key={project.id}
+              className={`${
+                projectClasses[index % projectClasses.length]
+              } shadow-lg rounded-lg overflow-hidden group relative`}
+            >
+              <img
+                src={project.image}
+                alt={project.title}
+                className='w-full h-full object-cover'
+              />
+              <div className='absolute inset-0 bg-charcoal backdrop-filter backdrop-blur-md bg-opacity-60 opacity-0 group-hover:opacity-100 flex justify-center items-center duration-500 ease-in-out'>
+                <div className='text-center text-putty'>
+                  <p className='text-sm md:text-xl font-medium'>
+                    {project.title}
+                  </p>
+                  <p className='mt-2 text-xs md:textlg'>
+                    {project.categories.join(", ")}
+                  </p>
+                </div>
               </div>
+              <button
+                onClick={() => openModal(project)}
+                className='absolute inset-0 w-full h-full z-10 focus:outline-none transition duration-700'
+              ></button>
             </div>
-            <button onClick={() => openModal(project)} className="absolute inset-0 w-full h-full z-10 focus:outline-none transition duration-700"></button>
-          </div>
-        ))}
+          ))}
+        </div>
+        {selectedProject && (
+          <Modal
+            project={selectedProject}
+            isOpen={!!selectedProject}
+            onRequestClose={closeModal}
+          />
+        )}
       </div>
-      {selectedProject && (
-        <Modal project={selectedProject} isOpen={!!selectedProject} onRequestClose={closeModal} />
-      )}
-    </div>
     </div>
   );
 };
